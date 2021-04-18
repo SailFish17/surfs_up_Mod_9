befa8f488868beb1a682fe686128f8c5ea083bc1
SailFish17 <philipafishman@gmail.com>
2021-04-17 22:42:59 -0400

    Module 9.5.6 all routes finished.

--- app.py
+++ app.py
@@ -1,22 +1,104 @@
-# 1. import Flask
-from flask import Flask
+# import dependencies
+import datetime as dt
+import numpy as np
+import pandas as pd
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from flask import Flask, jsonify
 
-# 2. Create an app, being sure to pass __name__
+####################
+#Set up the database
+####################
+
+# set up access and query ability to db
+engine = create_engine("sqlite:///hawaii.sqlite")
+
+# reflect the db into the classes.
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+
+# create variables 
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+#link python to the db
+session = Session(engine)
+
+#set up Flask, create Flask application call "app"
 app = Flask(__name__)
 
-# 3. Define what to do when a user goes to the index route
-@app.route("/")
-def home():
-    # print("Server received request for 'Home' page...")
-    return "Hello World for the second time"
+#add routes
+@app.route('/')
+
+#add welcome message to landing page
+#this shows as all one row.   That is incorrect.  Not sure why?
+def welcome():
+    return( 
+    '''
+    Welcome to the Climate Analysis API!
+    Available Routes:
+    /api/v1.0/precipitation
+    /api/v1.0/stations
+    /api/v1.0/tobs
+    /api/v1.0/temp/start/end
+    ''')
 
 
-# 4. Define what to do when a user goes to the /about route
-@app.route("/about")
-def about():
-    print("Server received request for 'About' page...")
-    return "Welcome to my 'About' page!"
+#Route 2 - precipitation from Mod 9.5.3
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    precipitation = session.query(Measurement.date, Measurement.prcp).\
+      filter(Measurement.date >= prev_year).all()
+    precip = {date: prcp for date, prcp in precipitation}
+    return jsonify(precip)
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+#Route 3 - stations from Mod 9.5.4
+@app.route("/api/v1.0/stations")
+def stations():
+    results = session.query(Station.station).all()
+    stations = list(np.ravel(results))
+
+    return jsonify(stations=stations)
+
+# Route 4 Monthly temperature from Mod 9.5.5
+@app.route("/api/v1.0/tobs")
+def temp_monthly():
+    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    results = session.query(Measurement.tobs).\
+        filter(Measurement.station == 'USC00519281').\
+        filter(Measurement.date >= prev_year).all()
+    temps = list(np.ravel(results))
+    return jsonify(temps=temps)
+
+# Route 5 Statistics from Mod 9.5.6
+# statistics here will require additional code.
+
+@app.route("/api/v1.0/temp/<start>")
+@app.route("/api/v1.0/temp/<start>/<end>")
+
+# create function called stats
+def stats(start=None, end=None):
+    sel = [func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
+
+    if not end:
+        results = session.query(*sel).\
+            filter(Measurement.date >= start).\
+            filter(Measurement.date <= end).all()
+        temps = list(np.ravel(results))
+        return jsonify(temps=temps)
+
+    results = session.query(*sel).\
+        filter(Measurement.date >= start).\
+        filter(Measurement.date <= end).all()
+    temps = list(np.ravel(results))
+    return jsonify(temps=temps)
+    
+        
+
+
+
+
